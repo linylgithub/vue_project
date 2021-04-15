bde003765cb7d3fcad13e31df44d43023f223a77
yllin <lylyoung@outlook.com>
2021-04-15 23:30:44 +0800

    plugin

---
+++ learn_vue_02/01-webpack的 使用/05-webpack的plugin/src/vue/Cpn.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>我是CPN组件的标题</h2>
+    <p>我是CPN组件的内容，哈哈哈哈哈</p>
+    <h2>{{name}}</h2>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  name: 'Cpn',
+  data() {
+    return {
+      name: '我是CPN'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
