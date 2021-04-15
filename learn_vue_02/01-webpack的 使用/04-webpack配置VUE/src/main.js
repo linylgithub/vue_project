6815fde10846cdb3373d416b0528f584bce8e1b9
yllin <username@users.noreply.github.com>
2021-04-15 17:36:35 +0800

    04

---
+++ learn_vue_02/01-webpack的 使用/04-webpack配置VUE/src/main.js
@@ -0,0 +1,52 @@
+const {add, mul} = require('./js/mathUtils.js')
+
+
+console.log(add(20, 30));
+console.log(mul(20, 30));
+
+import {name, age, height} from './js/info';
+
+console.log(name);
+console.log(age);
+console.log(height);
+
+
+// 3.依赖css文件
+require('./css/normal.css')
+
+// 4.依赖less文件
+require('./css/special.less')
+document.writeln('<h2>你好啊，VUE</h2>')
+
+// webpack
+// npm install webpack@3.6.0
+
+// css-loader
+// npm install css-loader@2.0.2 --save-dev
+
+// style-loader
+// npm install style-loader@0.23.1 --save-dev
+
+// less-loader
+// npm install --save-dev less-loader@4.1.0 less@3.9.0
+
+// url-loader
+// npm install --save-dev url-loader@1.1.2
+
+// npm uninstall --save-dev file-loader@3.0.1
+
+// es6转换成es5
+// npm install --save-dev babel-loader@7.1.5 babel-core@6.26.3 babel-preset-es2015@6.24.1
+
+// Vue
+// npm install vue@2.5.21 --save
+
+// 使用vue进行开发
+import Vue from 'vue'
+
+const app = new Vue({
+  el: "#app",
+  data: {
+    message: '你好啊'
+  }
+})
