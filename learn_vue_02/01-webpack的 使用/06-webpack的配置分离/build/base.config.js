4a4e6f37eef75619b2122297388568f45e535993
yllin <username@users.noreply.github.com>
2021-06-03 17:34:00 +0800

    config

---
+++ learn_vue_02/01-webpack的 使用/06-webpack的配置分离/build/base.config.js
@@ -0,0 +1,73 @@
+
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'bundle.js',
+    // publicPath: 'dist/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        // css-loader只负责加载
+        // style-loader负责将样式添加到DOM中
+        // 使用多个loader时，是从右向左
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.less$/,
+        use: [{
+          loader: "style-loader" // creates style nodes from JS strings
+        }, {
+          loader: "css-loader" // translates CSS into CommonJS
+        }, {
+          loader: "less-loader" // compiles Less to CSS
+        }]
+      },
+      {
+        test: /\.(png|jpg|gif|jpeg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              // 当加载的图片小于limit时，会将图片编译成base64字符串形式
+              // 当加载的图片大于limit时，需要使用file-loader模块进行加载
+              limit: 30000,
+              name: 'img/[name].[hash:8].[ext]'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015']
+          }
+        }
+      },
+      {
+        test: /\.vue$/,
+        use: ['vue-loader']
+      }
+    ],
+
+  },
+  plugins: [
+    new webpack.BannerPlugin('最终版权归yllin所有'),
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    })
+  ],
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  }
+}
---
+++ learn_vue_02/01-webpack的 使用/06-webpack的配置分离/src/vue/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h2 class="title">{{message}}</h2>
+    <button @click="btnClick">按钮</button>
+    <h2>{{name}}</h2>
+    <Cpn></Cpn>
+  </div>
+</template>
+
+<script>
+import Cpn from './Cpn.vue'
+export default {
+data() {
+    return {
+      message: '你好啊',
+      name: 'Joh'
+    }
+  },
+  methods: {
+    btnClick() {
+      console.log('hi vue!');
+    }
+  },
+  components: {
+    Cpn
+  }
+}
+</script>
+
+<style>
+ .title {
+   color: chartreuse;
+ }
+</style>
